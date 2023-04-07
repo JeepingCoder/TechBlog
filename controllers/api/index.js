3b44c40e761fa62e289629e0561737c4af5c0277
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-04-07 16:26:21 -0400

    fixed api index.js had wrong data in it

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,13 @@
 const router =require('express').Router();
 
-// will need user post comment routes 
+
+// this is our main set up for paths
 const userRoutes = require('./userRoutes');
+const homeRoutes = require('./ homeRoutes');
 
 
-//router.use for each
 router.use('/users', userRoutes);
+router.use('./api', apiRoutes);
 
 
 module.exports= router;
