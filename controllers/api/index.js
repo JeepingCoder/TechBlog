1a19e17b2812550e6e24d57c42cccf4e2f4ea443
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-04-07 14:34:14 -0400

    adding file structure, git ignore, install sqlize and handlebars packages

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router =require('express').Router();
 
-// change these when establish what i am using
+// will need user post comment routes 
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
 
-// change these when establish what i am uning
+
+//router.use for each
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+
 
 module.exports= router;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,3 @@
 // set up the required models
+const user =require('./user');
+const post = require('./post');
